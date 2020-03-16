2ab3842bd65add8ee226ec26c9aa6f9481b239ec
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-15 21:43:48 -0700

    commit

--- album_bot.py
+++ album_bot.py
@@ -8,6 +8,7 @@ from telegram_util import log_on_fail
 from telegram import InputMediaPhoto
 import os
 import web_2_album
+from PIL import Image
 
 with open('CREDENTIALS') as f:
     CREDENTIALS = yaml.load(f, Loader=yaml.FullLoader)
@@ -25,14 +26,25 @@ def getUrl(msg):
 
 @log_on_fail(debug_group)
 def toAlbum(update, context):
+	print(1)
 	msg = update.message
 	url = getUrl(msg)
 	imgs, cap = web_2_album.get(url)
+	if not imgs:
+		if msg.chat_id > 0:
+			msg.reply_text('can not find images in your url')
+		return
+	if 'bot_rotate' in msg.text:
+		for index, img_path in enumerate(imgs):
+		    img = Image.open(img_path)
+		    img = img.rotate(180)
+		    img.save(img_path)
+		    img.save('tmp_image/%s.jpg' % index)
 	group = [InputMediaPhoto(open(imgs[0], 'rb'), caption=cap, parse_mode='Markdown')] + \
 		[InputMediaPhoto(open(x, 'rb')) for x in imgs[1:]]
 	msg.reply_media_group(group, timeout = 20*60)
 
-tele.dispatcher.add_handler(MessageHandler(Filters.text & Filters.entity(URL), toAlbum))
+tele.dispatcher.add_handler(MessageHandler(Filters.text & Filters.entity('url'), toAlbum))
 
 tele.start_polling()
 tele.idle()
