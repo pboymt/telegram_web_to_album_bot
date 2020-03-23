42ebc930635e48ce5741ca4fd26543fdd916144f
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-22 17:17:50 -0700

    commit

--- album_bot.py
+++ album_bot.py
@@ -5,10 +5,7 @@ from telegram.ext import Updater, MessageHandler, Filters
 
 import yaml
 from telegram_util import log_on_fail
-from telegram import InputMediaPhoto
-import os
 import web_2_album
-from PIL import Image
 import weibo_2_album
 import twitter_2_album
 
@@ -26,43 +23,30 @@ def getUrl(msg):
 				url = "https://" + url
 			return url
 
-def getImageAndCap(url, msg):
+def getResult(url, msg):
 	# TODO: optimization based on url
 	if 'force_web' in msg.text:
 		return web_2_album.get(url)
 	if 'force_weibo' in msg.text:
 		return weibo_2_album.get(url)
 
-	candidate = [], ''
-	for method in [web_2_album, weibo_2_album, twitter_2_album]:
+	for method in [weibo_2_album, twitter_2_album, web_2_album]:
 		try:
-			new_candidate = method.get(url)
+			candidate = method.get(url)
 		except:
 			continue
-		candidate = web_2_album.compare(candidate, new_candidate)
-		if candidate[0]:
+		if not candidate.empty():
 			return candidate
-	return candidate
 
 @log_on_fail(debug_group)
 def toAlbum(update, context):
 	msg = update.effective_message
 	url = getUrl(msg)
-	imgs, cap = getImageAndCap(url, msg)
-
-	if 'bot_rotate' in msg.text:
-		for index, img_path in enumerate(imgs):
-			img = Image.open(img_path)
-			img = img.rotate(180)
-			img.save(img_path)
-			img.save('tmp_image/%s.jpg' % index)
-	
-	if imgs:			
-		group = [InputMediaPhoto(open(imgs[0], 'rb'), caption=cap, parse_mode='Markdown')] + \
-			[InputMediaPhoto(open(x, 'rb')) for x in imgs[1:]]
-		tele.bot.send_media_group(msg.chat_id, group, timeout = 20*60)
-	elif cap:
-		tele.bot.send_message(msg.chat_id, cap, parse_mode='Markdown')
+	result = getResult(url, msg)
+	if not result:
+		return
+	rotete = 'bot_rotate' in msg.text
+	album_sender.send(msg.chat, url, result, rotate = rotate)
 
 def test(update, context):
 	print(update.message.text_markdown)
